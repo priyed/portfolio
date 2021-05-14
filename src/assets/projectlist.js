78c231993a950b889bd2d3e525460cb74f279467
priyed <stanleyduye@gmail.com>
2021-05-14 06:37:18 +0100

    added foodies choice project

--- src/assets/projectlist.js
+++ src/assets/projectlist.js
@@ -1,36 +1,36 @@
 const projectList = [
-    {
-      id: 1,
-      title: 'Portfolio',
-      description: 'Built using react and custom css. I used a minimalist approach to the design and made it as lightweight as possible',
-      live: 'https://priyeduye.netlify.app',
-      code: `https://github.com/priyed/portfolio`,
-      
-    },
-    {
-        id: 2,
-        title: 'Priyed Gallery',
-        description: 'An image gallery application built with react and tailwindcss using the pixabay API for fetching images. It also includes a mechanism for users to search for images and an option to switch display mode.',
-        live: 'https://priyedgallery.netlify.app',
-        code: `https://github.com/priyed/priyedgallery`,
-        
-      },
-      {
-        id: 3,
-        title: 'Insure',
-        description: 'A landing page built with html, css and javascript as part of a series of challenges by codementor.',
-        live: 'https://insurepriyed.netlify.app',
-        code: `https://github.com/priyed/insure-landing-page`,
-        
-      },
-     
-      {
-        id: 4,
-        title: 'YSecure',
-        description: 'Web Template for a home security company, built with react, vanilla css. Also fitted react-hook-form and yup to handle form validation.',
-        live: 'https://ysecure.netlify.app/',
-        code: `https://github.com/priyed/home_secure`,
-        
-      },
-  ];
-  export default projectList;
+  {
+    id: 1,
+    title: "Foodies choice",
+    description:
+      "This is a functional food e-commerce application built using react, vanilla css and material UI. The site uses the commercejs API written in nodejs for backend functionality.",
+    live: "https://foodieschoice.netlify.app",
+    code: `https://github.com/priyed/foodieschoice`,
+  },
+  {
+    id: 2,
+    title: "Portfolio",
+    description:
+      "Built using react and custom css. I used a minimalist approach to the design and made it as lightweight as possible",
+    live: "https://priyeduye.netlify.app",
+    code: `https://github.com/priyed/portfolio`,
+  },
+  {
+    id: 3,
+    title: "Priyed Gallery",
+    description:
+      "An image gallery application built with react and tailwindcss using the pixabay API for fetching images. It also includes a mechanism for users to search for images and an option to switch display mode.",
+    live: "https://priyedgallery.netlify.app",
+    code: `https://github.com/priyed/priyedgallery`,
+  },
+
+  {
+    id: 4,
+    title: "YSecure",
+    description:
+      "Web Template for a home security company, built with react, vanilla css. Also fitted react-hook-form and yup to handle form validation.",
+    live: "https://ysecure.netlify.app/",
+    code: `https://github.com/priyed/home_secure`,
+  },
+];
+export default projectList;
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -45,85 +45,65 @@ const Navigation = () => {
 
   return (
     <div className="navigation" style={{ top: visible ? "0" : "-70px" }}>
-
       {/* Mobile Navigation */}
       <div className="menu-wrap">
-      <div id="main" className="lock-scroll">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          onClick={openNav}
-          className="menubtn"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={1.5}
-            d="M4 8h16M4 16h16"
-          />
-        </svg>
-      </div>
-      <nav id="mySidenav" className="sidenav">
-        <a href="#c" className="closebtn" onClick={closeNav}>
+        <div id="main" className="lock-scroll">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke="currentColor"
+            onClick={openNav}
+            className="menubtn"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
               strokeWidth={1.5}
-              d="M6 18L18 6M6 6l12 12"
+              d="M4 8h16M4 16h16"
             />
           </svg>
-        </a>
-        <ul id="menu">
-          <li>
-            <a className="link" href="#home" onClick={closeNav}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a className="link" href="#about" onClick={closeNav}>
-              About
-            </a>
-          </li>
-          <li>
-            <a className="link" href="#projects" onClick={closeNav}>
-              Projects
-            </a>
-          </li>
-          <li>
-            <a className="link" href="#contact" onClick={closeNav}>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav> 
-      </div>
-      
-      
-      {/*
-      <div class="menu-wrap">
-        <input type="checkbox" class="toggler" />
-        <div class="hamburger"><div></div></div>
-        <div class="menu">
-            <div>
-                <div>
-                    <ul>
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#projects">Projects</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
         </div>
-    </div> */}
+        <nav id="mySidenav" className="sidenav">
+          <a href="#c" className="closebtn" onClick={closeNav}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.5}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </a>
+          <ul id="menu">
+            <li>
+              <a className="link" href="#home" onClick={closeNav}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="link" href="#about" onClick={closeNav}>
+                About
+              </a>
+            </li>
+            <li>
+              <a className="link" href="#projects" onClick={closeNav}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a className="link" href="#contact" onClick={closeNav}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
       {/*Desktop Navigation */}
       <nav className="desktop-navigation">
